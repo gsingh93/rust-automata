88938031f69721d3ee0796561edfb26bb9bb5b26
Gulshan Singh <gulshan@umich.edu>
2015-03-14 01:43:12 -0400

    Implemented DFA iterator

--- src/dfa.rs
+++ src/dfa.rs
@@ -17,6 +17,56 @@ impl<S, I> DFA<S, I> {
     }
 }
 
+pub struct DFAIter<'a, S: 'a, I: 'a> {
+    input: Vec<I>,
+    transitions: &'a HashMap<(S, I), S>,
+    pos: usize,
+    cur_state: &'a S
+}
+
+impl<'a, S: 'a + Hash + Eq + Copy, I: Hash + Eq + Copy> Iterator for DFAIter<'a, S, I> {
+    type Item = &'a S;
+
+    fn next(&mut self) -> Option<&'a S> {
+        if self.pos > self.input.len() {
+            None
+        } else if self.pos == self.input.len() {
+            self.pos += 1;
+            Some(self.cur_state)
+        } else {
+            let c = self.input[self.pos];
+            match self.transitions.get(&(*self.cur_state, c)) {
+                Some(s) => {
+                    self.pos += 1;
+                    let ret = self.cur_state;
+                    self.cur_state = &s;
+                    Some(ret)
+                },
+                None => {
+                    // Watch out for overflow
+                    assert!(self.input.len() < self.input.len() + 1);
+
+                    // Skip the rest of the input
+                    self.pos = self.input.len() + 1;
+
+                    Some(self.cur_state)
+                }
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>){
+        (self.input.len(), Some(self.input.len()))
+    }
+}
+
+impl<S, I> DFA<S, I> {
+    pub fn iter(&self, input: Vec<I>) -> DFAIter<S, I> {
+        DFAIter { input: input, transitions: &self.transitions, cur_state: &self.start, pos: 0 }
+    }
+
+}
+
 impl<S, I> Automaton for DFA<S, I> where S: Hash + Eq + Copy, I: Hash + Eq + Copy {
     type State = S;
     type Alphabet = I;
@@ -77,12 +127,28 @@ mod test {
     #[test]
     fn test_dfa() {
         let transitions = map!((0, 'a') => 0, (0, 'b') => 1, (1, 'a') => 0, (1, 'b') => 2);
-        let dfa = DFA::new(1, set!(2), transitions);
+        let dfa = DFA::new(0, set!(2), transitions);
         assert_eq!(dfa.run("aaaaa".chars().collect()), None);
         assert_eq!(dfa.run("aabaa".chars().collect()), None);
         assert_eq!(dfa.run("aababbb".chars().collect()), None);
         assert_eq!(dfa.run("aababb".chars().collect()), Some(2));
         assert_eq!(dfa.run("aabb".chars().collect()), Some(2));
-        dfa.output_graphviz("dfa.dot");
+    }
+
+    #[test]
+    fn test_iter() {
+        let transitions = map!((0, 'a') => 0, (0, 'b') => 1, (1, 'a') => 0, (1, 'b') => 2);
+        let dfa = DFA::new(0, set!(2), transitions);
+        let mut it = dfa.iter("aababbb".chars().collect());
+        assert_eq!(it.next(), Some(&0));
+        assert_eq!(it.next(), Some(&0));
+        assert_eq!(it.next(), Some(&0));
+        assert_eq!(it.next(), Some(&1));
+        assert_eq!(it.next(), Some(&0));
+        assert_eq!(it.next(), Some(&1));
+        assert_eq!(it.next(), Some(&2));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
+        assert_eq!(it.next(), None);
     }
 }
